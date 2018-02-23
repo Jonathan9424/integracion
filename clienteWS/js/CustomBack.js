783aff6826b2802f6d63e32c8901b4da1bf5dfcb
Jonathan Maluche <andresanchez23@hotmail.com>
2018-02-23 11:25:51 -0500

    generacion cliente nuevos cambios

--- clienteWS/js/CustomBack.js
+++ clienteWS/js/CustomBack.js
@@ -1,8 +1,28 @@
-var back = function() {
-  var valorUno = document.getElementById("uno");
-  var valorDos = document.getElementById("dos");
+document.getElementById("boton").onclick = function() {
+  var valorUno = document.getElementById("uno").value;
+  var valorDos = document.getElementById("dos").value;
 
-  var res = valorUno + "" + valorDos;
-
-  document.getElementById("respuesta").innerHTML = "<p>" + res + "</p>";
+  valorUno = parseInt(valorUno);
+  valorDos = parseInt(valorDos);
+  var res = suma(valorUno, valorDos);
+  document.getElementById("resultado").innerHTML = res;
 };
+
+function suma(valorUno, valorDos) {
+  return valorUno + valorDos;
+}
+
+function creaAjax() {
+  var obj = new XMLHttpRequest();
+  obj.open("POST", "", true);
+  obj.onreadystatechange = function() {
+    if (obj.readyState == 4) {
+      if (obj.status == 200) {
+        var elemento = document.createElement(
+          "<p>" + obj.responseText + "</p>"
+        );
+        document.getElementById("resultado").firstChild.appendChild(elemento);
+      }
+    }
+  };
+}
